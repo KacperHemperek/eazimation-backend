fcea3f35a4efa4566a9f0cbe21211e9b65ba2f50
Kacper Hemperek <kacper.hemperek@o2.pl>
2024-04-16 15:12:21 +0200

    fix: set domain of cookie to top level domain

--- internal/auth/utils.go
+++ internal/auth/utils.go
@@ -7,31 +7,31 @@ import (
 
 func SetSessionCookie(w http.ResponseWriter, sessionID string) {
 	http.SetCookie(w, &http.Cookie{
-		Domain:   getUrlWithoutProtocol(frontendUrl, isProd),
 		Name:     SessionCookieName,
 		Value:    sessionID,
+		Domain:   getCookieDomain(),
 		Path:     "/",
 		MaxAge:   int(time.Hour * 24 * 7),
-		Secure:   false,
+		Secure:   isProd,
 		HttpOnly: true,
 	})
 }
 
 func RemoveSessionCookie(w http.ResponseWriter) {
 	http.SetCookie(w, &http.Cookie{
-		Domain:   getUrlWithoutProtocol(frontendUrl, isProd),
 		Name:     SessionCookieName,
 		Value:    "",
+		Domain:   getCookieDomain(),
 		Path:     "/",
 		MaxAge:   -1,
-		Secure:   false,
+		Secure:   isProd,
 		HttpOnly: true,
 	})
 }
 
-func getUrlWithoutProtocol(url string, secure bool) string {
-	if secure {
-		return url[len("https://"):]
+func getCookieDomain() string {
+	if isProd {
+		return "kacperhemperek.com"
 	}
-	return url[len("http://"):]
+	return "localhost"
 }
