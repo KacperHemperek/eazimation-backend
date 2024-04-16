fc79e965105b8bf97942f26ee49ee955cce6c315
Kacper Hemperek <kacper.hemperek@o2.pl>
2024-04-16 22:41:12 +0200

    fix: set domain of cookie from env variables

--- internal/auth/utils.go
+++ internal/auth/utils.go
@@ -2,14 +2,19 @@ package auth
 
 import (
 	"net/http"
+	"os"
 	"time"
+)
+
+var (
+	feDomain = os.Getenv("FRONTEND_DOMAIN")
 )
 
 func SetSessionCookie(w http.ResponseWriter, sessionID string) {
 	http.SetCookie(w, &http.Cookie{
 		Name:     SessionCookieName,
 		Value:    sessionID,
-		Domain:   getCookieDomain(),
+		Domain:   feDomain,
 		Path:     "/",
 		MaxAge:   int(time.Hour * 24 * 7),
 		Secure:   isProd,
@@ -21,17 +26,10 @@ func RemoveSessionCookie(w http.ResponseWriter) {
 	http.SetCookie(w, &http.Cookie{
 		Name:     SessionCookieName,
 		Value:    "",
-		Domain:   getCookieDomain(),
+		Domain:   feDomain,
 		Path:     "/",
 		MaxAge:   -1,
 		Secure:   isProd,
 		HttpOnly: true,
 	})
-}
-
-func getCookieDomain() string {
-	if isProd {
-		return "kacperhemperek.com"
-	}
-	return "localhost"
 }
